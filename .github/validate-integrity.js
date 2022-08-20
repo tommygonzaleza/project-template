7688a2901d6f911528dc8fb51acc54b9fb010841
Tomas <gonzalez.tommy2001@gmail.com>
2022-08-20 18:22:18 -0400

    Testing node-fetch (1)

--- .github/validate-integrity.js
+++ .github/validate-integrity.js
@@ -1,3 +1,10 @@
-console.log("Hello")
+import fetch from "node-fetch";
+
+async function getSchema(){
+    let response = await fetch("https://github.com/4GeeksAcademy/project-template/blob/main/.github/learn-schema.json");
+    let learnSchema = await response.json(); 
+    return learnSchema
+} 
+console.log(getSchema())
 
 process.exit(1);
